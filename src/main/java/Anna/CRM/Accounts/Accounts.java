68bfe56b954eec69a596e8ca616df6baeb48b9c0
Anna Timoshenko <atimoshenko@thomasmurray.com>
2022-02-22 17:58:41 +0300

    Fix adding accounts

--- src/main/java/Anna/CRM/Accounts/Accounts.java
+++ src/main/java/Anna/CRM/Accounts/Accounts.java
@@ -21,6 +21,12 @@ public class Accounts {
         this.email = email;
     }
 
+    public Accounts(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
 
     public Long getId() {
         return id;
